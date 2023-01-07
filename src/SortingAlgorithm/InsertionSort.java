b7420bea2ce2b9e925249b3f59d20eb281bce7e7
Ankit Kumar <ankit.kumar@newfold.com>
2023-01-08 03:11:33 +0530

    Implemented Insertion sort

--- src/SortingAlgorithm/InsertionSort.java
+++ src/SortingAlgorithm/InsertionSort.java
@@ -4,25 +4,21 @@ import java.util.Arrays;
 
 //space complexity o(1)
 //time complexity o(n2)
-public class BubbleSort {
+public class InsertionSort {
     public static void main(String[] args) {
         int[] arr = {-1,7,-32,0,89};
-        bubble(arr);
+        inserationSort(arr);
         System.out.println(Arrays.toString(arr));
     }
-    private static void bubble(int[] arr) {
-        boolean swap=false; //check when we sorted array as the input;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 1; j < arr.length-i; j++) {
-                if(arr[j]<arr[j-1]) {
+
+    private static void inserationSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j - 1];
                     arr[j - 1] = temp;
-                    swap=true;
                 }
-            }
-            if(!swap){
-                break;
             }
         }
     }
